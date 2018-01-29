c6c174d8cee4b0af1e190e9056f74b16ccf80503
Mika Morgan <35539259+mikamorgan@users.noreply.github.com>
2018-01-28 21:47:15 -0600

    Add comment blocks, update flipVert and flipHorz functions

--- assignments/program_1/main.cpp
+++ assignments/program_1/main.cpp
@@ -1,4 +1,14 @@
 /**
+* @ProgramName: Program-1
+* @Author: Mika Morgan 
+* @Description: 
+*     This program reads in images stored as rgb values in a space delimited file format.
+* @Course: 1063 Data Structures
+* @Semester: Spring 2018
+* @Date: 28 01 2018
+*/
+
+/**
 * TXT Image Manipulation Starter
 * 
 * This code is a simple way to read in color information stored in a space
@@ -29,6 +39,68 @@ struct rgb{
     int g;
     int b;
 };
+
+/**
+* @FunctionName: flipVert
+* @Description: 
+*     Loops through a 2D array and swaps the rows vertically.
+* @Params:
+*    rgb** image - 2D array holding rgb values
+*    int width - width of image
+*    int height - height of image
+* @Returns:
+*    void
+*/
+void flipVert(rgb** image,int width,int height){
+    int temp[width] = {0};
+
+for(int row = 0; row < (height / 2); row++){
+  for(int col = 0; col < width; col++){
+    temp[col] = image[row][col];
+    image[row][col] = image[height - 1 - row][col];
+    image[height - 1 - row][col] = temp[col];
+  }
+}
+}
+
+/**
+* @FunctionName: flipHorz
+* @Description: 
+*     Loops through a 2D array and swaps the columns horizontally.
+* @Params:
+*    rgb** image - 2D array holding rgb values
+*    int width - width of image
+*    int height - height of image
+* @Returns:
+*    void
+*/
+
+void flipHorz(rgb** image,int width,int height){
+    int temp[height] = {0};
+
+for(int col = 0; col < (width / 2); col++){
+   for(int row = 0; row < height; row++){
+    temp[row] = image[col][row];
+    image[col][row] = image[15 - 1 - col][row];
+    image[width - 1 - col][row] = temp[row];
+  }
+}
+}
+
+/**
+* @FunctionName: grayScale
+* @Description: 
+*     Loops through a 2D array and turns every RGB value into its grayscale equivalent.
+* @Params:
+*    rgb** image - 2D array holding rgb values
+*    int width - width of image
+*    int height - height of image
+* @Returns:
+*    void
+*/
+void grayScale(rgb** image,int width,int height){
+    // your code
+}
 
 int main(){
     ifstream ifile;          //Input / output files
