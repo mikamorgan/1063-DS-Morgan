debbbb937c314b5e99db131287671b7e1b046a52
Mika Morgan <35539259+mikamorgan@users.noreply.github.com>
2018-04-27 08:36:27 -0500

    Update main.cpp

--- assignments/program_3/main.cpp
+++ assignments/program_3/main.cpp
@@ -1,1 +1,62 @@
+/*
+ * @ProgramName: Program Three: Doubly Linked List
+ * @Author: Mika Morgan
+ * @Description: This program reads in a list of animal names from a file and counts the word length.
+ * It then multiplies the animal name length by a user given multiplier and traverses the list that number of times.
+ * The traversal moves right if the count is even and left otherwise.
+ * The program deletes the node it lands on, and then repeats this process until only one animal remains.     
+ * @Course: 1063 Data Structures
+ * @Semester: Spring 2018
+ * @Date: 26 04 2018
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "DBList.h"
+
+using namespace std;
+
+int main() {
+  DBList List;
+  string a;
+  int multiply;
+  int listCount = 0;
+  ifstream fin("animals.txt");
+  ofstream fout("eliminated.txt");
+    
+  //Step One: load a doubly-linked list with 
+  //animal names from the list
+    while(fin >> a){
+      List.Insert(a);
+      listCount++;
+    }
+    
+  //Step Two: reset the file  
+    fin.clear();
+    fin.seekg(0, std::ios::beg);
+    
+  //Step Three: get the multiplier
+    multiply = List.getMultiplier();
+    
+  //Steps Four-Eight: traverse the list and write out animal
+    fout << "First: ";
+    fin >> a;
+    fout << List.Traverse(a, multiply) << endl;
+    
+  //Step Nine: delete node
+    List.Delete(a);
+    listCount--;
+    
+  //Step Ten: repeat until only one animal remains
+    for(listCount; listCount > 1; listCount--){
+      fin >> a;
+      fout << "-" << List.Traverse(a, multiply) << endl;
+      List.Delete(a);
+      }
+  
+  //Step Eleven: declare the winner
+    fout << "Winner: ";
+    fout << List.Winner();
+}
 
